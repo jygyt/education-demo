d097c9dec8bbb4ad8e22bc23e8eaf9b5546eb193
liujibin <liujibin@yunzainfo.com>
2023-04-27 16:44:51 +0800

    删除多余文件

--- education/src/main/resources/data.sql
+++ education/src/main/resources/data.sql
@@ -1,2 +1,2 @@
-delete from `iot_device`.`auth` where id=1;
-INSERT INTO `iot_device`.`auth`(`id`, `account`, `created_auth_id`, `level`, `gmt_create`, `gmt_modified`, `password`, `lock`, `status_auth`, `refresh_token`) VALUES ('1', 'admin', '1', 0, '2020-10-27 09:26:47', '2020-10-27 09:26:47', '111111', 0, 0, NULL);
+delete from `education`.`auth` where id=1;
+INSERT INTO `education`.`auth`(`id`, `account`, `created_auth_id`, `level`, `gmt_create`, `gmt_modified`, `password`, `lock`, `status_auth`, `refresh_token`) VALUES ('1', 'admin', '1', 0, '2020-10-27 09:26:47', '2020-10-27 09:26:47', '111111', 0, 0, NULL);
